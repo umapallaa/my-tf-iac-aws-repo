49cf9515287465b0d53722ceb4d0dbc9f9d5428a
umapallaa <106008138+umapallaa@users.noreply.github.com>
2022-10-25 11:01:22 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
   description = "The AWS region to create things in."
-  default     = "us-east-2"
+  default     = "us-east-1"
 }
 
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     =  "myJune2021Key"
+  default     =  "uma"
 }
 
 variable "instance_type" {
@@ -15,15 +15,15 @@ variable "instance_type" {
 
 variable "security_group" {
   description = "Name of security group"
-  default     = "my-jenkins-security-group"
+  default     = "sg-081e10c1cb546c18c"
 }
 
 variable "tag_name" {
   description = "Tag Name of for Ec2 instance"
-  default     = "my-ec2-instance"
+  default     = "uma"
 }
 
 variable "ami_id" {
   description = "AMI for Ubuntu Ec2 instance"
-  default     = "ami-0b9064170e32bde34"
+  default     = "ami-08c40ec9ead489470"
 }
