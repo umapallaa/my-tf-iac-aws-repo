72ef1b06bb9e4504d17bb2c0ae7dbb01e03b626a
umapallaa <106008138+umapallaa@users.noreply.github.com>
2022-10-25 11:08:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 #Create security group with firewall rules
-resource "aws_security_group" "my_security_group" {
+resource "aws_security_group" "sg-081e10c1cb546c18c" {
   name        = sg-081e10c1cb546c18c
   description = "security group for Ec2 instance"
 
